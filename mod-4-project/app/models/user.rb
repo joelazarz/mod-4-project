81faf15b4dde7823b573e8be392806a1926e3f80
joe lazarz <jlazarz87@gmail.com>
2019-09-05 16:13:59 -0400

    relations set up

--- mod-4-project/app/models/user.rb
+++ mod-4-project/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :user_projects
+    has_many :user_tasks
     has_many :tasks, through: :user_tasks
     has_many :projects, through: :user_projects
     has_many :owned_projects, :class_name => 'Project', :foreign_key => 'user_id'
--- mod-4-project/db/seeds.rb
+++ mod-4-project/db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(name: "John Smith" , email: "john.smith@gmail.com", password: "12345", github_link: "https://github.com/", skills:"Javascript, Ruby")
+user2 = User.create(name: "Joe Lazarz", email:"jlazarz@gmail.com", password:"12345", github_link:"https://github.com/", skills:"Javascript, React")
+user3 = User.create(name: "Adrienne Miranda", email:"adrienne@gmail.com", password:"12345", github_link:"https://github.com/", skills:"Claw machine games, Javascript")
+
+project1 = Project.create(name:"Javascript Project", description:"project description", repo_link:"https://github.com/", languages:"Javascript", user_id:2)
+project2 = Project.create(name:"React Project", description:"project description", repo_link:"https://github.com/", languages:"React", user_id:3)
+
+user_project1 = UserProject.create(user_id:1, project_id:1)
+user_project2 = UserProject.create(user_id:2, project_id:1)
+user_project3 = UserProject.create(user_id:3, project_id:2)
+
+task1 = Task.create(description:"project description", status:"just started", project_id:1)
+task2 = Task.create(description:"project description", status:"half-way", project_id:2)
+task3 = Task.create(description:"project description", status:"almost done", project_id:1)
+task4 = Task.create(description:"project description", status:"finished", project_id:2)
+
+user_task1 = UserTask.create(user_id:1,task_id:1)
+user_task2 = UserTask.create(user_id:2,task_id:2)
+user_task3 = UserTask.create(user_id:1,task_id:3)
+user_task4 = UserTask.create(user_id:3,task_id:4)
