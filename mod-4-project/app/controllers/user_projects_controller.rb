b7461937f5dbfb5ce89c969e685c2696cea21127
adriennem76 <adrienne.c.miranda@gmail.com>
2019-09-09 18:02:09 -0400

    assign project/tasks

--- mod-4-project/app/controllers/user_projects_controller.rb
+++ mod-4-project/app/controllers/user_projects_controller.rb
@@ -1,2 +1,23 @@
 class UserProjectsController < ApplicationController
+
+  def index
+    user_projects = UserProject.all
+
+    render json: user_projects
+  end
+
+  def new
+    user_project = UserProject.new
+  end
+
+  def create
+    user_project = UserProject.create(user_project_params)
+    render json: user_project
+  end
+
+  private
+
+  def user_project_params
+      params.require(:user_project).permit(:user_id, :project_id)
+  end
 end
