150aa5731d1df6a8edfb987e8c568bbb39f87172
adriennem76 <adrienne.c.miranda@gmail.com>
2019-09-11 14:01:36 -0400

    project card fix

--- mod-4-project/app/serializers/user_serializer.rb
+++ mod-4-project/app/serializers/user_serializer.rb
@@ -3,4 +3,14 @@ class UserSerializer < ActiveModel::Serializer
   has_many :owned_projects
   has_many :projects
   has_many :tasks
+  def projects
+    self.object.projects.map do |project|
+      {id: project.id,
+      name: project.name,
+      repo_link: project.repo_link,
+      languages: project.languages,
+      user_id: project.user_id,
+      user: project.user}
+    end
+  end
 end
